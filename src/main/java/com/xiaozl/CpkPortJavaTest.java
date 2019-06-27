875d31e3dc7cf52e8682a17e3d398130dd6c691c
18390820674 <1445023633@qq.com>
2019-06-27 11:36:02 +0800

    s

--- src/main/java/com/xiaozl/CpkPortJavaTest.java
+++ src/main/java/com/xiaozl/CpkPortJavaTest.java
@@ -26,12 +26,11 @@ public class CpkPortJavaTest {
 	public static int[] a2 = new int[] {2,9,13,15,22,30};
 	public static int[] a3=  new int[] {1,2,6,12,16,18};
 	
-	
-	public static void count() {
+	//统计全部没有匹配到的值
+	public static int count(int sum) {
 		int got = 0;
 		int notGot = 0;
 		
-		int sum = 1;
 		for(int j= 0 ;j<list.size()-1;j++) {
 			int[] is = list.get(j); 
 			int[] up = list.get(j+1); 
@@ -39,12 +38,8 @@ public class CpkPortJavaTest {
 			for(int i = 0;i<is.length;i++) {
 				int he = is[i]+sum+i;
 				int jian = is[i]-(sum+i);
-				int cheng = is[i]*(sum+i);
-				int chu = is[i]/(sum+i);
-				
 				for(int k = 0;k<up.length;k++) {
-					if(he == up[k] || jian == up[k] || cheng ==up[k]|| chu==up[k]) {
-						System.out.println(up[k]); 
+					if(he == up[k] || jian == up[k] ) {
 						got++;
 					}else {
 						notGot++;
@@ -52,37 +47,49 @@ public class CpkPortJavaTest {
 				}
 			}
 		}
-		System.out.println("got"+got);
-		System.out.println("notGot"+notGot);
+		int bijiaocishu = (list.size()-1)*36;
+		if(notGot==bijiaocishu) {
+			return sum;
+		}
+		return 0;
 	}
 	
-	public static List<Integer>  getNum() {
+	//算出来的 不会要的值
+	public static List<Integer>  getNum(int sum) {
+		List<Integer> result = new ArrayList();
+		int[] is = list.get(list.size()-1); 
+		for(int i = 0;i<is.length;i++) {
+			int he = is[i]+sum+i;
+			int jian = is[i]-(sum+i);
+			for(int k = 1;k<=33;k++) {
+				Integer object = k; 
+				if(he == object || jian == object) {
+					result.add(object);
+				}
+			}
+		}
+		return result;
+	}
+	
+	//算出来需要的值
+	public static List<Integer>  getNeedNum() {
 		List<Integer> result = new ArrayList();
 		for(int k = 1;k<=33;k++) {
 			result.add(k);
 		}
-		
-		int[] is = list.get(list.size()-1); 
-		int sum = 1;
-		for(int i = 0;i<is.length;i++) {
-			int he = is[i]+sum+i;
-			int jian = is[i]-(sum+i);
-			int cheng = is[i]*(sum+i);
-			int chu = is[i]/(sum+i);
-			
-			System.out.print(he+",");
-			System.out.print(jian+",");
-			System.out.print(cheng+","); 
-			System.out.println(chu+","); 
-			
-			for(int k = 1;k<=33;k++) {
-				Integer object = k; 
-				if(he == object || jian == object || cheng ==object|| chu==object) {
+		//统计得到需要的sum值
+		for(int k = 1;k<=33;k++) {
+			int sum = count(k);	 
+			if(sum >0 ) {
+				System.out.print("sum"+sum+",");
+				//根据sum值筛选出相应的结果
+				List<Integer> num = new ArrayList<>();
+				num=getNum(sum); 
+				for(Integer object: num ) {
 					if(result.indexOf(object)>-1) {
 						result.remove(result.indexOf(object));
 					}
 				}
-				
 			}
 		}
 		return result;
@@ -90,11 +97,9 @@ public class CpkPortJavaTest {
 	
 	public static void main(String[] args) {
 		CpkPortJavaTest.init();
-		List<Integer> num = getNum(); 
-		for(Integer result: num ) {
-			System.out.print(result+",");
+		List<Integer> result = getNeedNum();
+		for(Integer object: result ) {
+			System.out.print(object+",");
 		}
 	}
-
-	
 }
